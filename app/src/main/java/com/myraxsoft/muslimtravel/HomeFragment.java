515154b118417f086c001d44338cbd7c91491ca4
shahrul <mshbo2017@gmail.com>
2019-03-06 00:40:23 +0800

    Develop UI

---
+++ app/src/main/java/com/myraxsoft/muslimtravel/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.myraxsoft.muslimtravel;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.content_main, container, false);
+        return myView;
+    }
+}
